89dfeda44721edae679af5fc7160bd851899270e
Adams Alves <alves.adams@gmail.com>
2021-09-06 18:29:39 -0300

    Add AlbumList to interface

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper-ad-api';
 
 const spotify = new SpotifyWrapper({
-  token: 'Token'
+  token: 'BQA1nF9UAC9U35fz5NDqs5ru3RbbEJLkztdAmL1A8Js295LvU9tWFzDZZaIXhhMmmXDzsbNruhG74e9OfPoJFDdV_qRGgWe6s2pHXQ0HpFu1zg4PSvZeO64XPERaSmIElh_bajGae8t_GBZE'
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Spotify Player!');
+import spotify from '../src/Spotify'
+import renderAlbums from './AlbumList'
+
+const albums = spotify.search.albums('Joy Division');
+const albumsList = document.getElementById('album-list');
+
+albums.
+  then(data => renderAlbums(data.albums.items, albumsList));
