aaf84782832c914090220d492f62c49a1c46b63f
Adams Alves <alves.adams@gmail.com>
2021-09-07 19:19:12 -0300

    Add playlistTrigger

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper-ad-api';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQCfP7N4DImXiE6KT3rKDCNcOvUu_A876HWnWpVZOL6HjBLsS1SbGdBaSUO8CGCznG1Nha3KXaJWKdLZi8WwgZlEL1e7o60XTAr0T2647zH0zhm10U_7-IvFDnF4oDF9oWp23SPYIir3EFfl'
+  token: 'BQAeIF_EodeQNJOjw1SS5oB9xbXEhUgTbTqrpmPkQ5Egw0TwNC1FYk_CCHmSoKG9pZAed-K_wDJYrNiOsXNCDBgaHxMdKXgFVyoQmAKlt372BCl67yr3vqrwoewLalYKmZNVqFsy_uU-oInY'
 });
 
 export default spotify;
